28799fc8007d1884e2beae6c1b55e31dca6a89cc
Sam <sam@tealstreet.io>
2026-04-29 18:16:15 -0700

    chore: OTA update v1 for Build 19

--- lib/version.ts
+++ lib/version.ts
@@ -4,8 +4,8 @@
 export const APP_VERSION = {
   version: "1.0.0", // Marketing version for app stores
   buildNumber: 19, // Native build number (increments only for native builds)
-  otaVersion: 0, // OTA update version (increments for JS updates)
-  timestamp: "2026-04-29T16:58:57Z", // Last update timestamp
+  otaVersion: 1, // OTA update version (increments for JS updates)
+  timestamp: "2026-04-30T01:16:15Z", // Last update timestamp
   channel: "testflight", // 'testflight' for TestFlight or 'production' for App Store
 };
 
